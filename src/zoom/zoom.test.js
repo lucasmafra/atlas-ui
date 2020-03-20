10ddeb9e4223f8b3658329718ed9af624b8d2a5e
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-20 12:36:54 -0300

    write up zoom tests

--- src/zoom/zoom.test.js
+++ src/zoom/zoom.test.js
@@ -1,7 +1,7 @@
 import * as nut from './zoom'
 
 describe('zoom', () => {
-  test('returns a matrix scaled and translated accordingly to given parameters', () => {
+  test('zooms at point - new matrix is scaled and translated around given point', () => {
     const matrix = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 }
     const scaleFactor = 2
     const point = { x: 5, y: 5 }
@@ -16,35 +16,23 @@ describe('zoom', () => {
   })
 
   describe('options', () => {
-    /*test('minZoom', () => {
+    test('preventZoomOutsideFigure', () => {
       const matrix = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 }
-      const scaleFactor = 0.5
-      const point = { x: 5, y: 5 }
-      const options = { minZoom: 0.75 }
-      expect(nut.zoom(matrix, scaleFactor, point, options)).toStrictEqual({
-        a: 1,
-        b: 0,
-        c: 0,
-        d: 1,
-        e: 0,
-        f: 0
-      })
-    })
+      const scaleFactor = 0.6
+      const point = { x: 2, y: 2 }
+      const options = { preventZoomOutsideFigure: true }
+      const svgDimensions = { width: 100, height: 100 }
+      const figureDimensions = { width: 200, height: 200 }
+      const context = { svgDimensions, figureDimensions }
 
-    test('maxZoom', () => {
-      const matrix = { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 }
-      const scaleFactor = 1.5
-      const point = { x: 5, y: 5 }
-      const options = { maxZoom: 1.25 }
-      expect(nut.zoom(matrix, scaleFactor, point, options)).toStrictEqual({
-        a: 1,
+      expect(nut.zoom(matrix, scaleFactor, point, options, context)).toStrictEqual({
+        a: 0.6,
         b: 0,
         c: 0,
-        d: 1,
+        d: 0.6,
         e: 0,
         f: 0
       })
     })
-*/
   })
 })
