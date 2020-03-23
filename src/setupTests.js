e7a11a2ff7a6122e2461853291d1525ff53579e9
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-22 21:50:27 -0300

    clean up

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
-import { tap } from './common-js/debug'
+import { tap, tapd } from './common-js/debug'
 global.tap = tap
+global.tapd = tapd
 require('jest-fetch-mock').enableMocks()
