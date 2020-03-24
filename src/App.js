27e012dc36ac25dd8b4048fd424dda0a135a9374
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-24 18:41:48 -0300

    Fetch diagram
    This commit makes the first step towards integrating Atlas UI to the api. We're fetching a mocked json from now, must implement calling the real api in a future commit.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { data } from './mock'
+import React, { useEffect, useState } from 'react'
+import { Spin } from 'antd'
 import 'antd/dist/antd.css'
 import SequenceDiagram from './sequence-diagram/SequenceDiagram'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [sequenceDiagram, setSequenceDiagram] = useState(null)
+
+  useEffect(() => {
+    fetch('/mock.json')
+      .then((r) => {
+        return r.json()
+      })
+      .then((data) => {
+        setSequenceDiagram(data)
+      })
+      .finally(() => {
+        setTimeout(() => setLoading(false), 1000)
+      })
+  }, [])
+
   return (
-    <div style={{ padding: 32, height: '100vh' }} data-testid={'sequence-diagram'}>
-      <SequenceDiagram data={data} />
+    <div
+      style={{
+        padding: 32,
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      data-testid={'sequence-diagram'}>
+      {loading ? <Spin aria-label='Loading' /> : <SequenceDiagram data={sequenceDiagram} />}
     </div>
   )
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import { render, act } from '@testing-library/react'
+import { render, act, wait } from '@testing-library/react'
 import App from './App'
 
-test('renders sequence diagram', async () => {
+const mockJson = {
+  lifelines: [{ name: 'serviceA' }],
+  executionBoxes: [],
+  arrows: [],
+  startTime: 0,
+  durationMs: 1000
+}
+
+fetch.mockResponse(JSON.stringify(mockJson))
+
+test('fetches json and renders diagram', async () => {
   await act(async () => {
-    const { getByTestId } = render(<App />)
-    expect(getByTestId('sequence-diagram')).toBeInTheDocument()
+    const { getByText, queryByLabelText } = await render(<App />)
+
+    await wait(() => {
+      expect(getByText('serviceA')).toBeInTheDocument()
+      expect(queryByLabelText('Loading')).toBe(null)
+    })
+  })
+})
+
+test('while is fetching render loader', async () => {
+  await act(async () => {
+    const { getByLabelText, queryByText } = await render(<App />)
+    expect(getByLabelText('Loading')).toBeInTheDocument()
+    expect(queryByText('serviceA')).toBe(null)
   })
 })
