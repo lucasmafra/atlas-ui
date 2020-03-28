9e181085d1ab3065e3be55a6794a318f37eb533f
Lucas Mafra <lucas.mafra95@gmail.com>
2020-03-28 15:07:34 -0300

    Add null safety test for snakeToCamel function

--- src/common-js/misc.test.js
+++ src/common-js/misc.test.js
@@ -1,14 +1,21 @@
 import * as nut from './misc'
 
-test('snakeToCamel', () => {
-  const myObj = {
-    attr_a: {
-      attr_b: [{ attr_c: 'do_not_convert' }]
+describe('snakeToCamel', () => {
+  test('camelize object keys', () => {
+    const myObj = {
+      attr_a: {
+        attr_b: [{ attr_c: 'do_not_convert' }]
+      }
     }
-  }
-  expect(nut.snakeToCamel(myObj)).toStrictEqual({
-    attrA: {
-      attrB: [{ attrC: 'do_not_convert' }]
-    }
+    expect(nut.snakeToCamel(myObj)).toStrictEqual({
+      attrA: {
+        attrB: [{ attrC: 'do_not_convert' }]
+      }
+    })
+  })
+
+  test('null safety', () => {
+    expect(nut.snakeToCamel(null)).toBe(null)
+    expect(nut.snakeToCamel({ attr_a: null })).toStrictEqual({ attrA: null })
   })
 })
