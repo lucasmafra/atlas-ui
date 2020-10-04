b1990d1aaa566bee9f18d2637d1c57fae5b16e63
davicorreiajr <davicorreiajr@gmail.com>
2020-10-04 18:14:34 -0300

    Improve AutoComplete and add two inputs to Sidebar (service and operation)

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Input } from 'antd';
+import { Input, Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import AutoComplete from '../widgets/AutoComplete';
 
 const SERVICES = [
@@ -22,16 +23,59 @@ const SERVICES = [
   }
 ]
 
+const OPERATIONS = [
+  {
+    id: 1,
+    name: 'GET HTTP'
+  },
+  {
+    id: 2,
+    name: 'POST HTTP'
+  },
+  {
+    id: 3,
+    name: 'graphql mutation'
+  },
+  {
+    id: 4,
+    name: 'consume'
+  },
+  {
+    id: 5,
+    name: 'produce'
+  }
+]
+
 const { Search } = Input;
 
+const StyledButton = styled(Button)`
+  justify-self: start;
+`
+
 const Container = styled.div`
-  width: 200px;
-  padding: 0px 4px;
+  display: grid;
+  grid-auto-flow: row;
+  gap: 16px;
+  align-content: start;
+  width: 300px;
+  padding: 0px 16px;
 `
 
 const Sidebar = () => {
-  const handleOnChange = (value) => {
-    console.log('search value: ', value)
+  const [form, setForm] = useState({ service: undefined, operation: undefined })
+
+  const handleOnChangeService = (service) => {
+    console.log('search value: ', service)
+    setForm({ ...form, service })
+  }
+
+  const handleOnChangeOperation = (operation) => {
+    console.log('search value: ', operation)
+    setForm({ ...form, operation })
+  }
+
+  const handleOnClick = () => {
+    console.log(form)
   }
 
   return (
@@ -39,8 +83,22 @@ const Sidebar = () => {
       <AutoComplete
         placeholder="Select a service"
         options={SERVICES}
-        onSelect={handleOnChange}
+        onSelect={handleOnChangeService}
+        label="Service"
       />
+      <AutoComplete
+        placeholder="Select an opeation"
+        options={OPERATIONS}
+        onSelect={handleOnChangeOperation}
+        label="Operation"
+      />
+      <StyledButton
+        type="primary"
+        icon={<SearchOutlined />}
+        onClick={handleOnClick}
+      >
+        Search
+      </StyledButton>
     </Container>
   )
 } 
--- src/widgets/AutoComplete.js
+++ src/widgets/AutoComplete.js
@@ -1,30 +1,39 @@
 import React, { useState } from 'react';
 import { Select } from 'antd';
+import styled from 'styled-components'
 
 const { Option } = Select;
 
-const AutoComplete = ({ options, onSelect, placeholder }) => {
+const Label = styled.p`
+  margin-bottom: 4px;
+  font-weight: bold;
+`
+
+const AutoComplete = ({ options, onSelect, placeholder, label }) => {
   const onChange = (id) => {
     console.log(`selected ${id}`);
     onSelect(id);
   }
   
   return (
-    <Select
-      allowClear
-      showSearch
-      style={{ width: '100%' }}
-      placeholder={placeholder}
-      optionFilterProp="children"
-      onChange={onChange}
-      filterOption={(input, option) =>
-        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-      }
-    >
-      {options.map(({ id, name }) => {
-        return <Option key={id} value={id}>{ name }</Option>
-      })}
-    </Select>
+    <div>
+      {label && <Label>{label}</Label>}
+      <Select
+        allowClear
+        showSearch
+        style={{ width: '100%' }}
+        placeholder={placeholder}
+        optionFilterProp="children"
+        onChange={onChange}
+        filterOption={(input, option) =>
+          option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+        }
+      >
+        {options.map(({ id, name }) => {
+          return <Option key={id} value={id}>{ name }</Option>
+        })}
+      </Select>
+    </div>
   )
 }
 
