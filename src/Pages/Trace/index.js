d751c76aad9fc35052ac88b30891335ea1e2b450
Lucas Mafra <lucas.mafra95@gmail.com>
2020-12-06 22:56:45 -0300

    add mocked analysis

--- src/Pages/Trace/index.js
+++ src/Pages/Trace/index.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Spin } from 'antd'
+import { Spin, Badge, Layout, Typography, Divider, Descriptions, Button } from 'antd'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import SequenceDiagram from '../../sequence-diagram/SequenceDiagramV2'
 import * as logParser from '../../sequence-diagram/log-parser'
 import { withNodeGroupId, collapseNodes, expandNodeGroup, collapseNodeGroup } from '../../sequence-diagram/node-grouping'
+import { CloseOutlined } from '@ant-design/icons';
 const parse = require('csv-parse/lib/sync')
+
+const { Title } = Typography;
 
 const StyledSpin = styled(Spin)`
   align-self: center;
@@ -63,16 +66,53 @@ const Trace = ({ onSelectNode, selectedNode }) => {
     })
   }
 
+  const [showAnalysis, setShowAnalysis] = useState(false)
+
+  const onShowAnalysis = () => setShowAnalysis(true)
+
+  const onHideAnalysis = () => setShowAnalysis(false)
+
   return loading ? (
     <StyledSpin aria-label='Loading' size='large' />
   ) : (
+    <Layout style={{ backgroundColor: 'white', width: '100vw'}}>
       <SequenceDiagram
         data={sequenceDiagram}
         onSelectNode={onSelectNode}
         selectedNode={selectedNode}
         onExpandNodeGroup={expand}
         onCollapseNodeGroup={collapse}
+        onShowAnalysis={onShowAnalysis}
       />
+      <Layout.Sider trigger={null} collapsible collapsed={!showAnalysis} collapsedWidth={0} style={{background: 'white'}} width={600}>
+
+        <div style={{padding: 16 }}>
+      <Descriptions title="Analysis" bordered  column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }} extra={<Button icon={<CloseOutlined />} onClick={onHideAnalysis}/>}>
+          <Descriptions.Item label="Total trace time">32m</Descriptions.Item>
+          <Descriptions.Item label="# of services">15</Descriptions.Item>
+          <Descriptions.Item label="# of requests">48</Descriptions.Item>
+          <Descriptions.Item label="# of messages">31</Descriptions.Item>
+          <Descriptions.Item label="Most requested service">warriv</Descriptions.Item>
+          <Descriptions.Item label="Slowest request">
+            Endpoint: POST /api/transfer-out/:id <br/>
+            Service: warriv <br/>
+            Total time: <span>1.35s</span> <br />
+          </Descriptions.Item>
+          <Descriptions.Item label="Slowest message">
+            Topic: TRANSFER-OUT-REQUESTED <br/>
+            Service: horadric <br/>
+            Total time: <span>3.1s</span> <br />
+          </Descriptions.Item>
+          <Descriptions.Item label="# errors by service">
+            stormshield: 1
+            <br />
+            pidgey: 2
+            <br />
+          </Descriptions.Item>
+        </Descriptions>
+        </div>
+      </Layout.Sider>
+    </Layout>
   )
 }
 
--- src/sequence-diagram/SequenceDiagramV2.js
+++ src/sequence-diagram/SequenceDiagramV2.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LifelineLabel from './LifelineLabel'
 import LifelineIcon from './LifelineIcon'
 import LifelineStreak from './LifelineStreak'
@@ -10,8 +10,10 @@ import NodeGroupMarker from './NodeGroupMarker';
 import useDimensions from "react-cool-dimensions";
 import { logProfile } from '../common-js/debug'
 import { getUngroupedNodes } from './node-grouping'
+import { Affix, Button, Tooltip } from 'antd';
+import { LineChartOutlined } from '@ant-design/icons';
 
-const SequenceDiagramV2 = ({ data, onSelectNode, selectedNode, onExpandNodeGroup, onCollapseNodeGroup }) => {
+const SequenceDiagramV2 = ({ data, onSelectNode, selectedNode, onExpandNodeGroup, onCollapseNodeGroup, onShowAnalysis }) => {
   const { lifelines, arrows, nodes, groupedNodes } = data
   const ungroupedNodes = getUngroupedNodes(nodes, groupedNodes)
 
@@ -77,12 +79,12 @@ const SequenceDiagramV2 = ({ data, onSelectNode, selectedNode, onExpandNodeGroup
 
   return (
     <div style={{ border: '1px solid #cecece', height: '100%', width: '100%', overflow: 'auto' }}>
-      <svg width={width + 100} height="138" style={{position: 'sticky', top: 0, background: 'white'}}>
+      <svg width={width + 120} height="138" style={{position: 'sticky', top: 0, background: 'white'}}>
         <g transform={`translate(${horizontalMargin}, ${verticalMargin})`}>
           {renderLifelinesHeader}
         </g>
       </svg>
-      <svg width={width + 100} height={height} ref={ref}>
+      <svg width={width + 120} height={height} ref={ref}>
         <g transform={`translate(${horizontalMargin}, ${-verticalMargin})`} >
           {renderLifelinesStreak}
           {renderNodes}
@@ -91,6 +93,11 @@ const SequenceDiagramV2 = ({ data, onSelectNode, selectedNode, onExpandNodeGroup
           {renderArrows}
         </g>
       </svg>
+      <Affix style={{ position: 'absolute', bottom: 32, right: 32 }}>
+        <Tooltip title="Analysis">
+          <Button type="primary" shape="circle" icon={<LineChartOutlined />} size={'large'} onClick={onShowAnalysis}/>
+        </Tooltip>
+      </Affix>
     </div>
   )
 }
